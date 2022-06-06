e77271fb0f1ea8cba3e7e629452a2c61974c2c8a
snarky-snark <snarky-snark@users.noreply.github.com>
2022-06-06 15:52:25 -0500

    Update Tutorial.svelte

--- src/components/widgets/Tutorial.svelte
+++ src/components/widgets/Tutorial.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <h3>how to play</h3>
-<div>Guess the <strong>WORDLE</strong> in {ROWS} tries.</div>
-<div>Each guess must be a valid {COLS} letter word. Hit the enter button to submit.</div>
+<div>Guess the <strong>ZOORDLE</strong> in {ROWS} tries.</div>
+<div>Each guess must be a valid animal-themed word. Hit the enter button to submit.</div>
 <div>
 	After each guess, the color of the tiles will change to show how close your guess was to the
 	word.
@@ -15,29 +15,29 @@
 <div class:complete={visible} class="examples">
 	<div><strong>Examples</strong></div>
 	<div class="row">
-		<Tile value="w" state="🟩" />
-		<Tile value="e" state="🔳" />
+		<Tile value="r" state="🟩" />
 		<Tile value="a" state="🔳" />
-		<Tile value="r" state="🔳" />
-		<Tile value="y" state="🔳" />
-	</div>
-	<div>The letter <strong>W</strong> is in the word and in the correct spot.</div>
-	<div class="row">
-		<Tile value="p" state="🔳" />
-		<Tile value="i" state="🟨" />
-		<Tile value="l" state="🔳" />
-		<Tile value="l" state="🔳" />
-		<Tile value="s" state="🔳" />
-	</div>
-	<div>The letter <strong>I</strong> is in the word but in the wrong spot.</div>
-	<div class="row">
 		<Tile value="v" state="🔳" />
-		<Tile value="a" state="🔳" />
-		<Tile value="g" state="🔳" />
-		<Tile value="u" state="⬛" />
 		<Tile value="e" state="🔳" />
+		<Tile value="n" state="🔳" />
 	</div>
-	<div>The letter <strong>U</strong> is not in the word in any spot.</div>
+	<div>The letter <strong>R</strong> is in the word and in the correct spot.</div>
+	<div class="row">
+		<Tile value="h" state="🔳" />
+		<Tile value="o" state="🔳" />
+		<Tile value="u" state="🔳" />
+		<Tile value="n" state="🔳" />
+		<Tile value="d" state="🟨" />
+	</div>
+	<div>The letter <strong>D</strong> is in the word but in the wrong spot.</div>
+	<div class="row">
+		<Tile value="g" state="🔳" />
+		<Tile value="e" state="🔳" />
+		<Tile value="c" state="🔳" />
+		<Tile value="k" state="⬛" />
+		<Tile value="o" state="🔳" />
+	</div>
+	<div>The letter <strong>K</strong> is not in the word in any spot.</div>
 </div>
 <div>
 	This is a recreation of the original <a
@@ -49,8 +49,10 @@
 	<br /><br />
 	Open the settings menu to see some of the additional features.
 	<br />
-	Written with Svelte, in Typescript by
+	Wordle+ is written with Svelte, in Typescript by
 	<a href="https://github.com/MikhaD" target="_blank">MikhaD</a>.
+	<br />
+	Zoordle is based on Wordle+ with superficial modifications.
 </div>
 
 <style lang="scss">
