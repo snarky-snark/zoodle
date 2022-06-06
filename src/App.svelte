d5778d0068ff00c34699c34b6a61ad64d6ecdf9b
snarky-snark <snarky-snark@users.noreply.github.com>
2022-06-06 14:41:32 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -90,7 +90,7 @@
 	}
 	let toaster: Toaster;
 
-	document.title = "Wordle+ | An infinite word guessing game";
+	document.title = "Zoordle | A word guessing game";
 </script>
 
 <Toaster bind:this={toaster} />
