249c2d472960a8b95a94eda0d1142fa36c56e1aa
snarky-snark <snarky-snark@users.noreply.github.com>
2022-06-06 15:40:02 -0500

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -46,7 +46,7 @@
 			toaster.pop(modeData.modes[$mode].name);
 		}}
 	>
-		wordle+
+		zoordle
 	</h1>
 	<div class="icons">
 		{#if showStats}
@@ -65,7 +65,7 @@
 	</div>
 	{#if tutorial}
 		<div transition:scale class="tutorial" on:click={() => dispatch("closeTutPopUp")}>
-			Tap WORDLE+ to change game mode
+			Tap ZOORDLE to change game mode
 			<span class="ok">OK</span>
 		</div>
 	{/if}
