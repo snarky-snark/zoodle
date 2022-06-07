5b85a7b6fb4ef0b14d7982b37e76505aabaa7203
snarky-snark <snarky-snark@users.noreply.github.com>
2022-06-07 15:07:02 -0500

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import seedrandom from "seedrandom";
 import { GameMode, ms } from "./enums";
-import wordList from "./animal_words_6";
+import wordList from "./animal_words";
 
 export const ROWS = 6;
 export const COLS = 5;
