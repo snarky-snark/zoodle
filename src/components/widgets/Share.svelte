35ed71564bea390ddf17949d380cc2b28ada7f72
snarky-snark <snarky-snark@users.noreply.github.com>
2022-06-08 12:04:11 -0500

    Fix typo in share string

--- src/components/widgets/Share.svelte
+++ src/components/widgets/Share.svelte
@@ -13,7 +13,7 @@
 	}/${state.board.words.length}\n\n    ${state.board.state
 		.slice(0, state.guesses)
 		.map((r) => r.join(""))
-		.join("\n    ")}\snarky-snark.github.io/zoodle`;
+		.join("\n    ")}\nsnarky-snark.github.io/zoodle`;
 </script>
 
 <div
