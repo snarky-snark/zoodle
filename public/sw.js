0a9839288039a8a47eab68a49bab1cac0a86c5cd
snarky-snark <snarky-snark@users.noreply.github.com>
2022-06-09 13:07:25 -0500

    Bump to v1.2.0

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const cacheName = "zoodle-v1.1.0";
+const cacheName = "zoodle-v1.2.0";
 
 const assetsToCache = [
 	"./",
